a928db5437b8d7223b0ec79db3b54143366038e9
Maxime <mongodinma@eisti.eu>
2014-04-09 13:59:16 +0200

    Update des utilisations de Calendar dans le main

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,12 +15,14 @@ public class Main {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		Date k = cal.getTime();
-		int k1= Calendar.DAY_OF_MONTH;
-		int k2 = Calendar.YEAR;
+		int k1= cal.get(Calendar.DAY_OF_MONTH);
+		int k2 = cal.get(Calendar.MONTH)+1;
+		int k3 = cal.get(Calendar.YEAR);
 	
 		System.out.println(k.getDate());
 		System.out.println(k1);
 		System.out.println(k2);
+		System.out.println(k3);
 		Utilisateur Jean = new Utilisateur("Jean","Dupont");
 		
 
