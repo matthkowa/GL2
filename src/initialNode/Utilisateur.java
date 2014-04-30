d969f0103dd2fda1521c18f2f99f20888eea157c
M4TtH13u <Administrator@172.16.11.194>
2014-04-30 12:29:23 +0200

    Ajout classe Serialiser

--- src/initialNode/Utilisateur.java
+++ src/initialNode/Utilisateur.java
@@ -1,77 +1,73 @@
 package initialNode;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import serializable.*;
 
-public class Utilisateur {
-private String nom;
-private String prenom;
-private Date dateNaissance;
-private String adresse;
-private int[] tel = new int[10] ;
-protected String pseudo;
-protected String motDePasse;
-
-
-
-public Utilisateur(String nom, String prenom, Date dateNaissance,
-		String adresse, int[] tel, String pseudo, String motDePasse) {
-	super();
-	this.nom = nom;
-	this.prenom = prenom;
-	this.dateNaissance = dateNaissance;
-	this.adresse = adresse;
-	this.tel = tel;
-	this.pseudo = pseudo;
-	this.motDePasse = motDePasse;
-}
-public String getAdresse() {
-	return adresse;
-}
-public void setAdresse(String adresse) {
-	this.adresse = adresse;
-}
-public int[] getTel() {
-	return tel;
-}
-public void setTel(int[] tel) {
-	this.tel = tel;
-}
-public String getPseudo() {
-	return pseudo;
-}
-public void setPseudo(String pseudo) {
-	this.pseudo = pseudo;
-}
-public String getMotDePasse() {
-	return motDePasse;
-}
-public void setMotDePasse(String motDePasse) {
-	this.motDePasse = motDePasse;
-}
-public String getNom() {
-	return nom;
-}
-public void setNom(String nom) {
-	this.nom = nom;
-}
-public String getPrenom() {
-	return prenom;
-}
-public void setPrenom(String prenom) {
-	this.prenom = prenom;
-}
-
-public Date getDateNaissance() {
-	return dateNaissance;
-}
-public void setDateNaissance(Date dateNaissance) {
-	this.dateNaissance = dateNaissance;
-}
-
-
-
-
+public class Utilisateur  extends Serialiser<Utilisateur>{
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private String adresse;
+	private int tel  ;
+	protected String pseudo;
+	protected String motDePasse;
+	
+	/*Constructor*/	
+	public Utilisateur(String nom, String prenom, Date dateNaissance,
+			String adresse, int[] tel, String pseudo, String motDePasse) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.adresse = adresse;
+		this.tel = tel;
+		this.pseudo = pseudo;
+		this.motDePasse = motDePasse;
+	}
+	
+	/*Getter and Setter*/
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int[] getTel() {
+		return tel;
+	}
+	public void setTel(int[] tel) {
+		this.tel = tel;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	
 }
---
+++ src/serializable/Serialiser.java
@@ -0,0 +1,15 @@
+package serializable;
+import java.io.Serializable;
+
+public class Serialiser implements Serializable{
+	
+	/*public T lecture(String path){
+		return (this.getClass()) "String";
+	}*/
+	public void sauvegarde(String path)
+	{
+		
+	}
+	
+	
+}
