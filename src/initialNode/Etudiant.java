69880f1f2724bf71839ed7cce7bc2596b9c60740
neosciz <Administrator@172.16.12.59>
2014-04-30 11:48:51 +0200

    Ajout d'étudiant et modification du readme

---
+++ src/initialNode/Etudiant.java
@@ -0,0 +1,14 @@
+package initialNode;
+
+import java.util.Date;
+
+public class Etudiant extends Utilisateur{
+
+	int idEtudiant;
+
+	public Etudiant(String nom, String prenom, Date dateNaissance, String adresse, int[] tel, String pseudo, String motDePasse) {
+		super(nom, prenom, dateNaissance, adresse, tel, pseudo, motDePasse);
+		this.idEtudiant=idEtudiant;
+	}
+
+}
