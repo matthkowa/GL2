9566a61636129bb41c6b1fb127b5112d3efa6d6d
hitboux <lesieurchr@eisti.eu>
2014-05-01 13:21:52 +0200

    Serializable complete avec un system de cryptage des données (sur toutes les données serializées)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,34 @@
 package main;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
 import java.util.Date;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+
 
 import initialNode.*;
 
 
 public class Main {
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws NoSuchAlgorithmException{
 		Administrateur Jean = new Administrateur("Jean", "Dupont", new Date(1,1,1), null, 0, null, null, 0);
 		Jean.addAdmin();
 		System.out.println("----Relecture----");
 		AllAdmin admin = new AllAdmin();
-		admin.relecture("Administrateur");
+		admin = (AllAdmin) admin.relecture("Administrateur");
 		for (Administrateur A : admin.administrateurs){
 			System.out.println(A.getNom()+" / "+A.getPrenom());
 		}
-
+		
+		
+		
 	}
-
 }
--- src/serializable/Serialiser.java
+++ src/serializable/Serialiser.java
@@ -1,14 +1,83 @@
 package serializable;
+import initialNode.Administrateur;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
 
 public class Serialiser implements Serializable{
+	
+	protected Serialiser(){}//constructor
 	
 	/*public T lecture(String path){
 		return (this.getClass()) "String";
 	}*/
-	public void sauvegarde(String path)
-	{
-		
+	
+	public void sauvegarder(String s){
+		try	{
+			Cipher cipher = Cipher.getInstance("DES");
+			SecretKey key = null;
+			try	{
+				FileInputStream f = new FileInputStream(new File("secretkey"));
+				ObjectInputStream ois = new ObjectInputStream(f);
+				key = ((SecretKey) ois.readObject());
+				ois.close();
+			}
+			catch (Exception e){
+				System.out.println("Erreur"+e);
+			}
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			SealedObject sealedObject = new SealedObject(this, cipher);
+			
+			FileOutputStream f = new FileOutputStream(new File(s));
+			ObjectOutputStream oos = new ObjectOutputStream(f);
+			oos.writeObject(sealedObject);
+			oos.close();
+			}
+			catch (Exception e){
+				System.out.println("Erreur "+e);
+			}
+	}
+	
+	
+	public Object relecture(String s) {
+		try	{
+
+			SecretKey key = null;
+			try	{
+				FileInputStream f = new FileInputStream(new File("secretkey"));
+				ObjectInputStream ois = new ObjectInputStream(f);
+				key = ((SecretKey) ois.readObject());
+				ois.close();
+			}
+			catch (Exception e){
+				System.out.println("Erreur"+e);
+			}
+			
+			FileInputStream f = new FileInputStream(new File(s));
+			ObjectInputStream ois = new ObjectInputStream(f);
+			Object o = ((Object) ois.readObject());			
+			SealedObject sealedObject = ((SealedObject) o);
+			String algorithmName = sealedObject.getAlgorithm();
+	        Cipher cipher = Cipher.getInstance(algorithmName);
+	        cipher.init(Cipher.DECRYPT_MODE, key);
+			ois.close();
+			o = (Object) sealedObject.getObject(cipher);
+			return o;
+			
+		}
+		catch (Exception e){
+			System.out.println("Erreur"+e);
+		}
+		return null;	
 	}
 	
 	
