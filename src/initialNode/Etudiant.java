d4a7b9b107e821f6782db084719412e36b7177c2
Neosciz <Administrator@172.16.12.59>
2014-05-09 12:07:43 +0200

    Ajout du début de la méthode CréerSession dans professeur Ajout de consulterResultat pour tous les étudiants

--- src/initialNode/Etudiant.java
+++ src/initialNode/Etudiant.java
@@ -6,7 +6,7 @@ import QCM.*;
 
 public class Etudiant extends Utilisateur{
 
-	int idEtudiant;
+	private int idEtudiant;
 
 	public Etudiant(String nom, String prenom, Date dateNaissance, String adresse, int tel, String pseudo, String motDePasse,int idEtudiant){
 		super(nom, prenom, dateNaissance, adresse, tel, pseudo, motDePasse);
@@ -30,7 +30,20 @@ public class Etudiant extends Utilisateur{
 		if (currentDate.compareTo(sess.getDateFin())==1){
 			Resultat res = new Resultat();
 			res = (Resultat) res.relecture("QCM/" + sess.getIdSession() + "/" + etu.idEtudiant);
-			
+			System.out.println(res);
+		}
+		else {
+			System.out.println("Vous n'êtes pas autorisé à consulter les résultats. Veuillez réessayer une fois la session terminée.");
+		}
+		
+	}
+	
+	public void consulterResultat(Etudiant etu){
+		Date currentDate = new Date();
+		if (currentDate.compareTo(sess.getDateFin())==1){
+			Resultat res = new Resultat();
+			res = (Resultat) res.relecture("QCM/*/" + etu.idEtudiant);
+			System.out.println(res);
 		}
 		else {
 			System.out.println("Vous n'êtes pas autorisé à consulter les résultats. Veuillez réessayer une fois la session terminée.");
--- src/initialNode/Professeur.java
+++ src/initialNode/Professeur.java
@@ -1,6 +1,9 @@
 package initialNode;
 
+import java.io.File;
 import java.util.*;
+
+import QCM.Session;
 
 public class Professeur extends Utilisateur {
 	private int idProf;
@@ -35,4 +38,26 @@ public class Professeur extends Utilisateur {
 		this.idProf = idProf;
 	}
 	
+	public Session CreerSession(){
+		Scanner sc = new Scanner(System.in);
+		String debutSess="";
+		String finSess="";
+		int i=0;
+		int repetition=1;
+		System.out.println("Veuillez entrer une date de début de session (MM/DD/YYYY)");
+		debutSess = sc.nextLine();
+		Date dateDebut = new Date(debutSess);
+		System.out.println("Veuillez entrer une date de fin de session (MM/DD/YYYY)");
+		finSess = sc.nextLine();
+		Date dateFin = new Date(finSess);
+		System.out.println("Combien de fois autorisez vous les étudiants à répondre à ce QCM?");
+		repetition = sc.nextInt();
+		Session sess = new Session(i, dateDebut, dateFin, repetition);
+		return sess;
+		/*File maSession = new File("QCM/");
+		String name = maSession.getName();
+		maSession.mkdir();*/
+		
+		
+	}
 }
