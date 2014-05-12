9656a6f477cd891628e7e2a68750ec64673868da
M4TtH13u <Administrator@M4TtH13u>
2014-05-13 00:14:11 +0200

    Ajout d'une fonction addAll à la classe All<T>

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -2,28 +2,20 @@ package controller;
 import initialNode.Administrateur;
 
 import java.io.IOException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+
 
 import view.menu.View;
 
 public class MenuController {
 	
-	
-	
-		
-		private void authentification(){
-			/*A ecrire */
-			//Authentification aut = new Authentification();/*Creer un constructeur dans authentification */
-			
-			
-				boolean b = true;
-				while(b)
-				{
-					try{
-						int carac = View.demandeInt("_______MENU 1______\n 1- Administrateur \n 2- Professeur \n 3- Etudiant \n 4- Quitter \n \n Choix :");
-						switch (carac){
-						case 1 :	
+	private void authentification(){
+		boolean b = true;
+		while(b)
+		{
+			try{
+				int carac = View.demandeInt("_______MENU 1______\n 1- Administrateur \n 2- Professeur \n 3- Etudiant \n 4- Quitter \n \n Choix :");
+				switch (carac){
+					case 1 :	
 						//	Administrateur a =aut.authentificationAdmin(); 
 						//	if (aut.getAutReussi()){
 								administrateur();
@@ -32,170 +24,155 @@ public class MenuController {
 								
 						//	}
 							b=false;
-						break;
-						case '2' :
+					break;
+					case 2 :
 							/*Professeur p = aut.authentificationProf(); if (aut.getAutReussi()){}else{
 					System.out.println("Retour");b=false;
 						}*/professeur();b=false;break;
-						case '3' :/* Etudiant e = aut.authentificationEtudiant(); if(aut.getAutReussi()){}else{	System.out.println("Retour ");b=false;}
-					}*/ etudiant();b=false;break;
-						case '4' : b=false; break;
-				
-						
-						default : break;
-					}
-					}
-					catch(IOException e){
-						System.out.println("Erreur");
-						
-					}
-					
-					
-			}
-		}
-		
-		private void administrateur2(int choix,Administrateur a){
-			
-				boolean b = true;
-				while(b){
-					try{
-						int carac = View.demandeInt("_______MENU 1______\n 1- Gérer les administrateur \n 2- Gérer les professeurs \n 3- Gérer les étudiants \n 4- Gérer les promotions \n 5- Gérer les modules \n 6- Deconnexion \n \n Choix :");
-					
-					if(carac==4) b= false; 
-					else{
-						switch(choix) {
-							case 1 : /*Administrateur*/
-								switch(carac) { 
-								case 1 : a.addAdmin(); 	break;
-								case 2 : a.modifAdmin();  	break;
-								case 3 : a.supAdmin();  break;
-								}
-							break;
-							
-							case 2 : /*Professeur*/
-								switch(carac) { 
-								case 1 : a.addProf(); 	break;
-								case 2 : a.modifProf();  	break;
-								case 3 : a.supProf(); 	break;
-								}
-							break;
-							case 3 :/*Etudiant*/
-								switch(carac) { 
-								case 1 : a.addEleve(); 	break;
-								case 2 : a.modifEleve();  	break;
-								case 3 : a.supEleve();  	break;
-								}
-							break;
-							case 4 :/*Promotion*/
-								switch(carac) { 
-								case 1 : /*Ajouter Promo*/ 		break;
-								case 2 : /*Modifier Promo*/  		break;
-								case 3 : /*Supprimer Promo*/  	break;
-								}
-							break;
-							case 5 :/*Modules*/
-								switch(carac) { 
-								case 1 : a.addModule(); 		break;
-								case 2 : a.modifModule();  	break;
-								case 3 : a.supModule();  	break;
-								}
-							break;
-							default : System.out.println("Erreur veuillez contacter le support"); break;
-						}
-					}
-				}catch(IOException e){
-					System.err.println("Problème : " + e);
+					case 3 :/* Etudiant e = aut.authentificationEtudiant(); if(aut.getAutReussi()){}else{	System.out.println("Retour ");b=false;}
+					}*/ etudiant();b=false;
+					break;
+					case 4 :
+						b=false; 
+					break;
+					default :
+					break;
 				}
 			}
-				
-		}
-		
-		private void administrateur(){
-			Administrateur a = new Administrateur();
-			boolean b = true;
-			while(b){
-				try{
-					int carac = View.demandeInt("_______MENU 1______\n 1- Gérer les administrateur \n 2- Gérer les professeurs \n 3- Gérer les étudiants \n 4- Gérer les promotions \n 5- Gérer les modules \n 6- Deconnexion \n \n Choix :");
-					if(carac == 1 || carac == 2 || carac == 3 || carac == 4 || carac == 5){
-						administrateur2(carac,a);
-					}
-					else{ 
-						if(carac == 6) b=false;
-					}
-				}
-				catch(IOException e){
-					System.out.println("Erreur");
-					
-				}
-				
-				
-				
-				
-			}
-		}
-			
-			
-		
-		private void professeur(){
-			boolean b = true;
-			while(b)
-			{
-				try{
-					int carac = View.demandeInt("_______MENU 1______\n 1- Gérer les QCM \n 2- Visualiser les résultats \n 3- Deconnexion \n \n Choix :");
-					switch (carac){
-					case '1' : break;
-					case '2' : break;
-					case '3' : b=false; break;				
-					default : break;
-				}
-				}
-				catch(IOException e){
-					System.out.println("Erreur");
-					
-				}
-		}
-			
-		}
-		private void etudiant(){
-			boolean b = true;
-			while(b)
-			{	
-				try{
-					int carac = View.demandeInt("_______MENU 1______\n 1- Répondre aux QCM \n 2- Visualiser les résultats \n 3- Deconnexion \n \n Choix :");
-					switch (carac){
-					case '1' : break;
-					case '2' : break;
-					case '3' : b=false; break;				
-					default : break;
-				}
-				}
-				catch(IOException e){
-					System.out.println("Erreur");
-					
-				}
-				
-			}
-		}
-		
-		public MenuController(){
-			boolean b = true;
-			while(b)
-			{
-				try{
-					int carac = View.demandeInt("_______MENU 1______\n 1- S'authentifier \n 2- Quitter \n \n Choix :");
-					switch (carac){
-					case 1 : authentification(); break;
-					case 2 :b=false; break;
-					default : break;
-					}
-				}
-				catch(IOException e){
-					System.out.println("Erreur");
-					
-				}
-				
+			catch(IOException e){
+				System.out.println("Erreur");						
 			}
 		}
 	}
+		
+	private void administrateur2(int choix,Administrateur a){		
+		boolean b = true;
+		while(b){
+			try{
+				int carac = View.demandeInt("_______MENU 1______\n 1- Gérer les administrateur \n 2- Gérer les professeurs \n 3- Gérer les étudiants \n 4- Gérer les promotions \n 5- Gérer les modules \n 6- Deconnexion \n \n Choix :");
+				if(carac==4) b= false; 
+				else{
+					switch(choix) {
+						case 1 : /*Administrateur*/
+							switch(carac) { 
+							case 1 : a.addAdmin(); 	break;
+							case 2 : a.modifAdmin();  	break;
+							case 3 : a.supAdmin();  break;
+							}
+						break;
+						
+						case 2 : /*Professeur*/
+							switch(carac) { 
+							case 1 : a.addProf(); 	break;
+							case 2 : a.modifProf();  	break;
+							case 3 : a.supProf(); 	break;
+							}
+						break;
+						case 3 :/*Etudiant*/
+							switch(carac) { 
+							case 1 : a.addEleve(); 	break;
+							case 2 : a.modifEleve();  	break;
+							case 3 : a.supEleve();  	break;
+							}
+						break;
+						case 4 :/*Promotion*/
+							switch(carac) { 
+							case 1 : /*Ajouter Promo*/ 		break;
+							case 2 : /*Modifier Promo*/  		break;
+							case 3 : /*Supprimer Promo*/  	break;
+							}
+						break;
+						case 5 :/*Modules*/
+							switch(carac) { 
+							case 1 : a.addModule(); 		break;
+							case 2 : a.modifModule();  	break;
+							case 3 : a.supModule();  	break;
+							}
+						break;
+						default : System.out.println("Erreur veuillez contacter le support"); break;
+					}
+				}
+			}catch(IOException e){
+				System.err.println("Problème : " + e);
+			}
+		}			
+	}
+	
+	private void administrateur(){
+		Administrateur a = new Administrateur();
+		boolean b = true;
+		while(b){
+			try{
+				int carac = View.demandeInt("_______MENU 1______\n 1- Gérer les administrateur \n 2- Gérer les professeurs \n 3- Gérer les étudiants \n 4- Gérer les promotions \n 5- Gérer les modules \n 6- Deconnexion \n \n Choix :");
+				if(carac == 1 || carac == 2 || carac == 3 || carac == 4 || carac == 5){
+					administrateur2(carac,a);
+				}
+				else{ 
+					if(carac == 6) b=false;
+				}
+			}
+			catch(IOException e){
+				System.out.println("Erreur");
+				
+			}
+		}
+	}		
+	
+	private void professeur(){
+		boolean b = true;
+		while(b)
+		{
+			try{
+				int carac = View.demandeInt("_______MENU 1______\n 1- Gérer les QCM \n 2- Visualiser les résultats \n 3- Deconnexion \n \n Choix :");
+				switch (carac){
+					case 1 : break;
+					case 2 : break;
+					case 3 : b=false; break;				
+				default : break;
+				}
+			}
+			catch(IOException e){
+				System.out.println("Erreur");
+			}
+		}
+	}
+	
+	private void etudiant(){
+		boolean b = true;
+		while(b)
+		{	
+			try{
+				int carac = View.demandeInt("_______MENU 1______\n 1- Répondre aux QCM \n 2- Visualiser les résultats \n 3- Deconnexion \n \n Choix :");
+				switch (carac){
+					case 1 : break;
+					case 2 : break;
+					case 3 : b=false; break;				
+				default : break;
+				}
+			}
+			catch(IOException e){
+				System.out.println("Erreur");					
+			}				
+		}
+	}
+	
+	public MenuController(){
+		boolean b = true;
+		while(b)
+		{
+			try{
+				int carac = View.demandeInt("_______MENU 1______\n 1- S'authentifier \n 2- Quitter \n \n Choix :");
+				switch (carac){
+					case 1 : authentification(); break;
+					case 2 :b=false; break;
+					default : break;
+				}
+			}
+			catch(IOException e){
+				System.out.println("Erreur");					
+			}				
+		}
+	}
+}
 	
 
--- src/initialNode/All.java
+++ src/initialNode/All.java
@@ -53,6 +53,14 @@ public class All<T> extends Serialiser implements Serializable{
 	}
 	
 	/**
+	 * Ajoute un set d'objets T à l'attribut set (set d'objet T)
+	 * @param Collection objet de type T
+	 */
+	public void addAll(Collection<T> c){
+		this.set.addAll(c);
+	}
+	
+	/**
 	 * supprime un objet T à l'attribut set (set d'objet T)
 	 * @param objet objet de type T
 	 */
