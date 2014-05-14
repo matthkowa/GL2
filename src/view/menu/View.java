956633fbe3962ee2e9d246a656b92aedeae20334
neosciz <Administrator@172.16.12.59>
2014-05-14 11:48:24 +0200

    bug

--- src/view/menu/View.java
+++ src/view/menu/View.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 public class View {
 	/**
-	 * Met en forme les menus
+	 * Met en forme les menus 
 	 * @param s String à afficher sur la console
 	 */
 	public static void affichageEnteteMenu(String s){
